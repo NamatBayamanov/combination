d7c40986240c3e74dd35a17e3623b7c46875b96c
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-16 08:53:43 +0600

    added change

--- src/components/ArticleList/ArticleUpdate/ArticleUpdate.js
+++ src/components/ArticleList/ArticleUpdate/ArticleUpdate.js
@@ -28,6 +28,7 @@ function ArticleUpdate() {
 
     const formData = new FormData(event.target);
     // alert(`${formData.get("title")} ${formData.get("description")}`);
+    
     axios.put(`https://combination-309b1-default-rtdb.firebaseio.com/articles/${id}.json`, Object.fromEntries(formData.entries())).then(response => navigate(`/${id}`))
   }
 
